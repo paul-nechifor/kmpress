2eb3c819ab8ec8b5630e4a67386376ec4cb29309
Paul Nechifor <paul@nechifor.net>
2014-05-14 12:29:57 +0300

    Add generator for Voronoi images.

---
+++ examples/colors/colors.cc
@@ -0,0 +1,26 @@
+#include "voro++.hh"
+using namespace voro;
+
+// Set up constants for the container geometry
+const double x_min=-5,x_max=5;
+const double y_min=-5,y_max=5;
+const double z_min=0,z_max=10;
+
+// Set up the number of blocks that the container is divided into.
+const int n_x=6,n_y=6,n_z=6;
+
+int main() {
+	// Create a container with the geometry given above, and make it
+	// non-periodic in each of the three coordinates. Allocate space for
+	// eight particles within each computational block.
+	container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
+			false,false,false,8);
+
+	con.import("list");
+
+	// Save the Voronoi network POV-Ray format.
+	con.draw_cells_pov("colors_v.pov");
+
+	// Output the particles in POV-Ray format.
+	con.draw_particles_pov("colors_p.pov");
+}
--- readme.md
+++ readme.md
@@ -13,9 +13,22 @@ another][facetrain]). The requirements are from [here][ex9].
     mkdir -p "`pwd`/lib"
     go get ./...
 
+## Examples
+
+### Voronoi
+
+You have to install [Voro++][voro] and [POV-Ray][povray].
+
+Build the executable:
+
+    cd examples/colors
+    make
+
 ## License
 
 MIT
 [course]: http://thor.info.uaic.ro/~ciortuz/teaching.html
 [facetrain]: https://github.com/paul-nechifor/facetrain
 [ex9]: http://openclassroom.stanford.edu/MainFolder/DocumentPage.php?course=MachineLearning&doc=exercises/ex9/ex9.html
+[voro]: http://math.lbl.gov/voro++/download/
+[povray]: http://www.povray.org/download/
