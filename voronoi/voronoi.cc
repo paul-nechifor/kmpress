8162c4ab15968c8f1c868602bfc90aaa437ecc4a
Paul Nechifor <paul@nechifor.net>
2014-05-16 01:34:35 +0300

    Use file names from arguments.

--- voronoi/voronoi.cc
+++ voronoi/voronoi.cc
@@ -1,4 +1,5 @@
 #include "voro++.hh"
+#include <iostream>
 using namespace voro;
 
 // Set up constants for the container geometry
@@ -9,18 +10,23 @@ const double z_min=-5,z_max=5;
 // Set up the number of blocks that the container is divided into.
 const int n_x=6,n_y=6,n_z=6;
 
-int main() {
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    std::cout << "Usage: list colors_v.pov colors_p.pov" << std::endl;
+    return 1;
+  }
+
 	// Create a container with the geometry given above, and make it
 	// non-periodic in each of the three coordinates. Allocate space for
 	// eight particles within each computational block.
 	container con(x_min,x_max,y_min,y_max,z_min,z_max,n_x,n_y,n_z,
 			false,false,false,8);
 
-	con.import("list");
+	con.import(argv[1]);
 
 	// Save the Voronoi network POV-Ray format.
-	con.draw_cells_pov("colors_v.pov");
+	con.draw_cells_pov(argv[2]);
 
 	// Output the particles in POV-Ray format.
-	con.draw_particles_pov("colors_p.pov");
+	con.draw_particles_pov(argv[3]);
 }
