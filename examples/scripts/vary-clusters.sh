6e7083372a234ad136d0dfbb967af08ed57243ef
Paul Nechifor <paul@nechifor.net>
2014-05-17 02:49:07 +0300

    Move to own dir.

--- examples/scripts/vary-clusters.sh
+++ examples/scripts/vary-clusters.sh
@@ -4,8 +4,10 @@ set -e
 
 here="`dirname $0`"
 kmpress="$here/../../kmpress/kmpress"
-results="$here/../results"
+results="$here/../results/vary-clusters"
 image="$here/../images/bird_large.tiff"
+
+mkdir -p "$results" 2>/dev/null
 
 for i in {2..100}; do
   $kmpress -clusters $i -i $image -o "$results/bird-seq-$i.tiff"
